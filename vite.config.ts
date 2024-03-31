c4e7991544928cdf945923b03bef118e802b957a
iktnb <iknowthatinobody@gmail.com>
2024-03-31 20:44:28 +0300

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./",
+  base: "/vue3-vite-ts-test/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src")
